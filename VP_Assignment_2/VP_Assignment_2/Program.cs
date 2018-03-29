2d9208a201de787ea27a8272a505e859b299763d
rehan <rehanmuhammad08@gmail.com>
2018-03-30 01:27:05 +0500

    -Main Scree

--- VP_Assignment_2/VP_Assignment_2/Program.cs
+++ VP_Assignment_2/VP_Assignment_2/Program.cs
@@ -16,7 +16,7 @@ namespace VP_Assignment_2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new markAttendance());
+            Application.Run(new Welcome());
         }
     }
 }
