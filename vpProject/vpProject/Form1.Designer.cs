df7d589744074c02c7c35ac31c884eb713779daa
rehan <rehanmuhammad08@gmail.com>
2018-04-21 09:52:33 +0500

    -Prototype

---
+++ vpProject/vpProject/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace vpProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Trends = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MintCream;
+            this.label1.Location = new System.Drawing.Point(82, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(266, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Twitter Trends Spy";
+            // 
+            // Trends
+            // 
+            this.Trends.ForeColor = System.Drawing.Color.Black;
+            this.Trends.Location = new System.Drawing.Point(176, 133);
+            this.Trends.Name = "Trends";
+            this.Trends.Size = new System.Drawing.Size(138, 53);
+            this.Trends.TabIndex = 5;
+            this.Trends.Text = "Check TwitterTrends";
+            this.Trends.UseVisualStyleBackColor = true;
+            this.Trends.Click += new System.EventHandler(this.Trends_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            this.button2.Location = new System.Drawing.Point(30, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 53);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Login";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(320, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 53);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Show Graph";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(194, 213);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 42);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(481, 331);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Trends);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Aqua;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Investigate Twitter Suspicious Trends";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Trends;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ vpProject/vpProject/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vpProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Login obj = new Login();
+            obj.Show();
+        }
+
+        private void Trends_Click(object sender, EventArgs e)
+        {
+            Trends obj = new Trends();
+            obj.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            showGraphs obj = new showGraphs();
+            obj.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
